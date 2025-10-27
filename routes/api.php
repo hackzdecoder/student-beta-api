f6ff3a7eef20efafcd730b8ce5e2075f052e7e77
masterdev090195 <hackzdecoder@gmail.com>
2025-10-28 07:25:09 +0800

    Login API Structure

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,11 @@
 
 use App\Http\Controllers\api\attendance\AttendanceController;
 use App\Http\Controllers\api\auth\AuthenticationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthenticationController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () { 
-    Route::get('/logout', [AuthenticationController::class, 'logout']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
     Route::get('/attendance', [AttendanceController::class, 'attendance']);
 });
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
